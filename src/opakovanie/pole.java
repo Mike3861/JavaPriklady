3c13ff92fb4a527dd1d0c48d4725ebbba1ff40d7
Mike <mike.fekete02@gmail.com>
2023-04-19 17:18:54 +0200

    Školenie

--- src/opakovanie/pole.java
+++ src/opakovanie/pole.java
@@ -3,19 +3,15 @@ package opakovanie;
 public class pole {
     public static void main(String[] args) {
 
-        int[] cisla = { 2, 7, 12, 5, 8, 11, 0 };
-        System.out.println("____________________________");
+        int [] cisla = { 2, 7, 12, 5, 8, 11, 0 };
         System.out.println("cisla delitelne dvoma/parne:");
-        System.out.println("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
         for (int i = 0; i < cisla.length; i++) {
             if (cisla[i] % 2 == 0) {
-                System.out.print(cisla[i]+ " ");
+                System.out.print(cisla[i]+ " " );
             }
 
         }
-        System.out.println("\n¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
         System.out.println("\ncisla nedelitelne dvoma/neparne:");
-        System.out.println("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
         for (int i = 0; i < cisla.length; i++) {
             if (cisla[i] % 2 != 0) {
                 System.out.print(cisla[i]+ " ");
